35b4c5ea0f8c6e93b17b49b2443ddc6f3a3759b3
Hoang <leminhhoang912006@gmail.com>
2026-06-11 16:34:03 +0700

    Icon Updating

--- FE/src/components/layout/Dashboard/Dashboard.jsx
+++ FE/src/components/layout/Dashboard/Dashboard.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar.jsx";
 import Navbar from "./Navbar.jsx";
-
 import "./Dashboard.css";
 
 function Dashboard() {
