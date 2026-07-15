338e0689772fb85db9c502a00be2d8f2c8a05486
dangkjoabi80Âgmail.com <dangkhoabi80@gmail.com>
2026-07-15 14:43:19 +0700

    feature: allow user change password

--- BE/src/routes/authRoutes.js
+++ BE/src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
+const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/google', authController.googleLogin);
 router.post('/verify-otp', authController.verifyOTP);
@@ -12,6 +13,8 @@ router.post('/logout', authController.logout);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/verify-reset-otp', authController.verifyResetPasswordOTP);
 router.post('/reset-password', authController.resetPassword);
+router.post('/change-password', authMiddleware, authController.changePassword);
+router.delete('/account', authMiddleware, authController.deleteAccount);
 router.get("/search", authController.searchUsers);
 router.get("/users/:id/profile", authController.getUserProfileById);
 
